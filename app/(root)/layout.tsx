60b94ebf0706970dbf5aeaae6a1a3ddcc7db4b7d
minhng-178 <anhminh1782002@gmail.com>
2023-12-23 10:04:57 +0700

    layout page

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,16 @@
+import Footer from '@/components/shared/Footer';
+import Header from '@/components/shared/Header';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex h-screen flex-col">
+      <Header />
+      <body>{children}</body>
+      <Footer />
+    </div>
+  );
+}
