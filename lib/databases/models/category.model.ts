eea433e49882f88e621695650869e389affc627d
minhng-178 <anhminh1782002@gmail.com>
2023-12-25 09:24:50 +0700

    fix api

--- lib/databases/models/category.model.ts
+++ lib/databases/models/category.model.ts
@@ -1,4 +1,4 @@
-import { Schema, model, models } from 'mongoose';
+import { Document, Schema, model, models } from 'mongoose';
 
 export interface ICategory extends Document {
   _id: string;
--- lib/databases/models/user.model.ts
+++ lib/databases/models/user.model.ts
@@ -2,12 +2,13 @@ import { Schema, model, models } from 'mongoose';
 
 const UserSchema = new Schema({
   clerkId: { type: String, required: true, unique: true },
-  email: { type: String, require: true, unique: true },
-  username: { type: String, require: true },
-  firstName: { type: String, require: true },
-  lastName: { type: String, require: true },
+  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  photo: { type: String, required: true },
 });
 
-const User = models.User || model('Users', UserSchema);
+const User = models.User || model('User', UserSchema);
 
 export default User;
